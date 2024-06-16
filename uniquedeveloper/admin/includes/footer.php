ae61143a19b1a3eb7f1a14823b20e419feb4415f
Muhammad Baldy <muhammadbaldy693@gmail.com>
2024-06-16 19:08:03 +0700

    initial commit

---
+++ uniquedeveloper/admin/includes/footer.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <style type="text/css">
+        .context-dark, .bg-gray-dark, .bg-primary {
+            color: rgba(0, 0, 0, 0.8) !important;
+        }
+
+        .footer-classic a, .footer-classic a:focus, .footer-classic a:active {
+            color: #000000 !important;
+        }
+
+        .nav-list li {
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
+
+        .nav-list li a:hover:before {
+            margin-left: 0;
+            opacity: 1;
+            visibility: visible;
+        }
+
+        ul, ol {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .social-inner {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            padding: 23px;
+            font: 900 13px/1 "Lato", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+            text-transform: uppercase;
+            color: rgba(0, 0, 0, 0.5) !important;
+        }
+
+        .social-container .col {
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            display: inline-block; 
+        }
+
+        .nav-list li a:before {
+            content: "\f14f";
+            font: 400 21px/1 "Material Design Icons";
+            color: #4d6de6;
+            display: inline-block;
+            vertical-align: baseline;
+            margin-left: -28px;
+            margin-right: 7px;
+            opacity: 0;
+            visibility: hidden;
+            transition: .22s ease;
+        }
+
+        .footer-classic {
+            background-color: white;
+            color: black !important;
+        }
+
+        .footer-classic .contact-list dt, .footer-classic .contact-list dd, .footer-classic .nav-list li a {
+            color: black !important;
+        }
+
+        .footer-classic h5 {
+            color: black !important;
+        }
+
+        .footer-classic p {
+            color: black !important;
+        }
+    </style>
+</head>
+<body>
+
+            <!---footer Section Start   ----->
+
+<br><br><br><br>
+
+<footer class="section footer-classic context-dark bg-image" style="background: white;">
+    <br><br>
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4">
+                <right><img src="https://pics.io/preview/66692131dd64cba0962f1f8c/thumbnail" height="150px" width="250px" alt="site logo"></right>
+                <p>Edu-Home Virtual Classroom is an excellent learning hub, dedicated to the programmer who want to explore.</p>
+                <!-- Rights-->
+                <p class="rights"><span>© </span><span class="copyright-year">Copyright</span><span> </span><span>Edu-Home</span><span>. </span><span>All Rights Reserved.</span></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5>Contacts</h5>
+              <dl class="contact-list">
+                <dt>Address:</dt>
+                <dd>Indonesia</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>email:</dt>
+                <dd><a href="mailto:#">afdillahfaqih@gmail.com</a></dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>phones:</dt>
+                <dd><a href="tel:#">+62 8889201364</a></dd>
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-3">
+              <h5>Links</h5>
+              <ul class="nav-list">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contacts</a></li>
+ 
